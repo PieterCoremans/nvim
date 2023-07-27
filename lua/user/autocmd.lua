f436526fcfaf2a58df5656f3e1dce359b91bc84c
pieter coremans <pieter@pietercoremans.xyz>
2023-07-27 20:00:54 +0200

    Finetunes autocmd and remap.

--- lua/user/autocmd.lua
+++ lua/user/autocmd.lua
@@ -1,15 +1,16 @@
 local map = vim.keymap.set
+local A = vim.api
 
 vim.api.nvim_create_autocmd({ "FileType " }, {
   pattern = { "lua", "sql" },
   callback = function()
  -- Lua/SQL comment/uncomment line
-  map("n", "<leader>lc", function()
+  map("n", "<leader>cl", function()
       local linestart = GetStart() if linestart == "--" then return "0xxx" else return "0i-- <Esc>" end
   end, { expr = true })
 
  -- Lua/SQL comment/uncomment block
-  map("n", "<leader>pc", function()
+  map("n", "<leader>cp", function()
       local linestart = GetStart() if linestart == "-- " then return "<C-v>}k:norm 0xxx <Esc>0j" else return "<C-v>}k:norm 0i-- <Esc>0j" end
   end, { expr = true })
 
@@ -26,15 +27,42 @@ vim.api.nvim_create_autocmd({ "FileType " }, {
     ]]
 
     -- Python comment/uncomment line
-  map("n", "<leader>lc", function()
+  map("n", "<leader>cl", function()
       local linestart = GetStart() if linestart == "# " then return "0xx" else return "0i# <Esc>" end
   end, { expr = true })
 
     -- Python comment/uncomment block
-  map("n", "<leader>pc", function()
+  map("n", "<leader>cp", function()
       local linestart = GetStart() if linestart == "# " then return "<C-v>}k:norm 0xx <Esc>0j" else return "<C-v>}k:norm 0i# <Esc>0j" end
   end, { expr = true })
 
 end,
 })
 
+local num_au = A.nvim_create_augroup('CUSTOM', { clear = true })
+
+-- Highlight the region on yank
+A.nvim_create_autocmd('TextYankPost', {
+    group = num_au,
+    callback = function()
+        vim.highlight.on_yank({ higroup = 'Visual' })
+        -- This is a workaround for clipboard not working in WSL
+        -- see https://github.com/neovim/neovim/issues/19204#issuecomment-1173722375
+        -- if vim.fn.has('wsl') == 1 then
+        --     vim.fn.system('clip.exe', vim.fn.getreg('"'))
+        -- end
+    end,
+})
+
+-- Remove useless stuff from the terminal window and enter INSERT mode
+A.nvim_create_autocmd('TermOpen', {
+    group = num_au,
+    callback = function(data)
+        if not string.find(vim.bo[data.buf].filetype, '^[fF][tT]erm') then
+            A.nvim_set_option_value('number', false, { scope = 'local' })
+            A.nvim_set_option_value('relativenumber', false, { scope = 'local' })
+            A.nvim_set_option_value('signcolumn', 'no', { scope = 'local' })
+            A.nvim_command('startinsert')
+        end
+    end,
+})
--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -14,18 +14,20 @@ map("n", "<leader>w", vim.cmd.w)
 map("n", "<leader>q", vim.cmd.q)
 
 -- Go to next/previous buffer
-map("n", "<leader>bn", vim.cmd.bn)
-map("n", "<leader>bp", vim.cmd.bp)
+-- map("n", "<leader>bn", vim.cmd.bn)
+-- map("n", "<leader>bp", vim.cmd.bp)
+map("n", "<leader>n", vim.cmd.bn)
+map("n", "<leader>p", vim.cmd.bp)
 map('n', '<leader>[', '<CMD>bp<CR>')
 map('n', '<leader>]', '<CMD>bn<CR>')
 map('n', '<Tab>', '<CMD>bn<CR>')
 map('n', '<S-Tab>', '<CMD>bp<CR>')
 
 -- List buffers
-map("n", "<leader>bl", "<cmd>Telescope buffers<CR>")
+map("n", "<leader>l", "<cmd>Telescope buffers<CR>")
 
 -- Close current buffer
-map("n", "<leader>bd", vim.cmd.bd)
+map("n", "<leader>d", vim.cmd.bd)
 
 -- Telescope keybinds
 map('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
@@ -35,18 +37,19 @@ map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 map('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
 map('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
 
--- local builtin = require('telescope.builtin')
--- map('n', '<C-f>', builtin.git_files, {})
+local builtin = require('telescope.builtin')
+map('n', '<C-f>', builtin.git_files, {})
 -- map('n', '<leader>fs', function()
 -- 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 -- end)
 
 -- Open terminal in horizontal split below
-map("n", "<leader>t", ":below sp<CR>:term<CR><cmd>resize 10<cr>i")
+-- map("n", "<leader>t", ":below sp<CR>:term<CR><cmd>resize 10<cr>i")
+map("n", "<leader>t", ":below sp<CR>:term<CR><cmd>resize 10<cr>")
 
 -- In terminal insert mode, go back to normal mode/go back to script
 map("t", "<C-n>", "<C-\\><C-N>")
-map("t", "<C-\\>", "<C-\\><C-N><C-w>k")
+map("t", "<C-\\>", "<C-\\><C-N><C-w>k") -- only works after <leader>t command
 
 -- map Ctrl+{h,j,k,l} to navigate windows
 -- from normal mode
@@ -70,22 +73,37 @@ map("n", "<leader>rl", "<cmd>resize<cr>")
 map("n", "<leader>o", "o<Esc>")
 map("n", "<leader>O", "O<Esc>")
 
--- Move selection 1 line higher/lower
-map("v", "J", ":m >+1<CR>gv=gv")
-map("v", "K", ":m <-2<CR>gv=gv")
+--Close brackets without return
+map('i', '(', '()<Left>')
+map('i', '{', '{}<Left>')
+map('i', '[', '[]<Left>')
+
+-- Move line up and down in NORMAL and VISUAL modes
+-- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
+-- map('n', '<C-j>', '<CMD>move .+1<CR>')
+-- map('n', '<C-k>', '<CMD>move .-2<CR>')
+-- map('x', '<C-j>', ":move '>+1<CR>gv=gv")
+-- map('x', '<C-k>', ":move '<-2<CR>gv=gv")
 
 -- Keep cursor in the middle of the page while doing page down/up
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 
 -- Yank to "+ register
-map({"n", "v"}, "<leader>y", '"+y')
-
+-- map({"n", "v"}, "<leader>y", '"+y')
 -- Paste from to "+ register
-map("n", "<leader>p", '"+p')
+-- map("n", "<leader>p", '"+p')
+
+--Copy selected text to system clipboard (requires gvim installed):
+--remap Ctrl+c to copy from vim to clipboard
+map('v', '<C-c>', '"+y')
+--remap Ctrl+x to cut from vim to clipboard
+map('v', '<C-x>', '"+d')
+--remap Ctrl+p to paste from clipboard to vim
+map('v', '<C-p>', '"+p')
 
 -- Keep paste buffer after paste
-map("x", "<leader>p", "\"_dP")
+-- map("x", "<leader>p", "\"_dP")
 
 -- Delete to black hole register
 map({"n", "x"}, "x", '"_x')
